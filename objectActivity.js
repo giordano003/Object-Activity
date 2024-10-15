962afa06fec446bd617297b361294bedfa34f4df
Jordan <ligadosjordan3@gmail.com>
2024-10-15 20:19:42 +0800

    Add files via upload

---
+++ objectActivity.js
@@ -0,0 +1,17 @@
+// Create an object called car with the properties type, model, and color.
+let car = {
+    type: "Sedan",
+    model: "Camry",
+    color: "Blue"
+};
+
+// Use typeof to check the type of the object and log it to the console.
+console.log("Type of car:", typeof car);
+
+// Update the type property to "Toyota" and log the updated object.
+car.type = "Toyota";
+console.log(car);
+
+// Add a new property wheels with the value 4 and log the updated object.
+car.wheels = 4;
+console.log(car);
